9730a636b7cc08a4119e22f734c5e2fa77cc1b9e
xinjiakun <xinjiakun@jd.com>
2019-11-06 20:00:14 +0800

    commit

--- src/com/xin/socket/ClientFile.java
+++ src/com/xin/socket/ClientFile.java
@@ -1,6 +1,9 @@
 package com.xin.socket;
 
-import java.io.*;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 /**
@@ -10,23 +13,23 @@ import java.net.Socket;
  * @description: TODO
  * @date 2019/11/5 21:26
  */
-public class Myclient {
+public class ClientFile {
     public static void main(String[] args) throws IOException {
         //连接服务器
         Socket socket = new Socket("127.0.0.1", 9999);
         InputStream inputStream = socket.getInputStream();
         //接收字节流
-        byte[] bs = new byte[100];
 
-        inputStream.read(bs);
-        System.out.println("client接收到" + new String(bs));
 
-        //发送字节流
-        OutputStream outputStream = socket.getOutputStream();
-        outputStream.write("myclient".getBytes());
+        byte[] bytes = new byte[100];
+        int len = 0;
+        OutputStream fileout = new FileOutputStream("D:\\workspace.xml");
+        while ((len = inputStream.read(bytes)) != -1) {
+            fileout.write(bytes, 0, len);
+        }
 
+        fileout.close();
         inputStream.close();
-        outputStream.close();
         socket.close();
     }
 }
--- src/com/xin/socket/SeverFile.java
+++ src/com/xin/socket/SeverFile.java
@@ -1,7 +1,6 @@
 package com.xin.socket;
 
 
-import javax.imageio.IIOException;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -13,7 +12,7 @@ import java.net.Socket;
  * @description: TODO
  * @date 2019/11/5 21:17
  */
-public class MySever {
+public class SeverFile {
     public static void main(String[] args) throws IOException {
         //定义服务端端口号
         ServerSocket sever = new ServerSocket(9999);
@@ -23,18 +22,18 @@ public class MySever {
 
         //发送字符串 hello
         OutputStream outputStream = socket.getOutputStream();
-        byte[] bs = new byte[100];
-        String info = "hello";
-        outputStream.write(info.getBytes());
-        //获取客户端发来的流  保存到字节流bs
-        InputStream inputStream = socket.getInputStream();
-        inputStream.read(bs);
-        System.out.println(new String(bs));
+        //发送本地文件
+        File file = new File("D:\\projects\\workspace.xml");
+        InputStream is = new FileInputStream(file);
+        byte[] filebytes = new byte[100];
+        int len = 0;
+        while ((len = is.read(filebytes)) != -1) {
+            outputStream.write(filebytes, 0, len);
+        }
 
-
+        is.close();
         sever.close();
         socket.close();
-        inputStream.close();
         outputStream.close();
     }
 }

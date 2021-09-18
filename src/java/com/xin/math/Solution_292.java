d395c9bee446fa49476ebacaf152704b3fbf254a
xinjiakun <1913829181@qq.com>
2021-09-18 21:19:25 +0800

    nim

---
+++ src/java/com/xin/math/Solution_292.java
@@ -0,0 +1,14 @@
+package com.xin.math;
+
+/**
+ * @author 辛佳锟
+ * @title: Solution_292
+ * @projectName LeetCode
+ * @description: Nim 游戏
+ * @date 2021/9/18 21:03
+ */
+public class Solution_292 {
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}

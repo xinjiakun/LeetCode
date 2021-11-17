d12290bec2a3ec76e23dde83659d1d39ecef0296
xinjiakun <1913829181@qq.com>
2021-11-17 23:23:54 +0800

    最大单词长度乘积

--- src/java/com/xin/list/Solution_318.java
+++ src/java/com/xin/list/Solution_318.java
@@ -1,5 +1,6 @@
 package com.xin.list;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -10,6 +11,29 @@ import java.util.Map;
  */
 public class Solution_318 {
     public int maxProduct(String[] words) {
-        Map<Integer,int[]> map = Enum
+        int[] data = new int[words.length];
+        for (int i = 0; i < words.length; i++) {
+            int x = 0;
+            for (int j = 0; j < words[i].length(); j++) {
+                x |= (1 << (words[i].charAt(j)-'a'));
+            }
+            data[i]=x;
+        }
+        int max = 0;
+        for (int i = 0; i < words.length; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                if ((data[i] & data[j]) > 0) {
+                    continue;
+                } else {
+                    max = Math.max(max, words[i].length() * words[j].length());
+                }
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        String[]a= {"a","aa","aaa","aaaa"};
+        new Solution_318().maxProduct(a);
     }
 }

13612ca149d9e39a0ac63eb3ff016f481dede101
xinjiakun <xinjiakun@jd.com>
2021-11-17 22:05:14 +0800

    最大单词长度乘积

---
+++ src/java/com/xin/list/Solution_318.java
@@ -0,0 +1,15 @@
+package com.xin.list;
+
+import java.util.Map;
+
+/**
+ * @Classname Solution_318
+ * @Description 最大单词长度乘积
+ * @Date 2021/11/17 22:00
+ * @Created by xinjiakun
+ */
+public class Solution_318 {
+    public int maxProduct(String[] words) {
+        Map<Integer,int[]> map = Enum
+    }
+}
